b1333a8e344080203052e7553a358876c80fbcce
shivam1081 <68644986+shivam1081@users.noreply.github.com>
2022-01-21 08:21:26 +0530

    first commit

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+    // ! Here Instead of using separate States we can create a single state having object and the key value pairs.
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: "",
+    //     enteredAmount: "",
+    //     enteredDate: "",
+    // });
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+        // setUserInput({ ...userInput, enteredTitle: event.target.value });
+        //Instead of using this we should update using a function as it will prevent the problem caused by scheduled rendering.
+
+        // setUserInput = (prevState) => {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // };
+    };
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+        //     setUserInput({ ...userInput, enteredAmount: event.target.value });
+        //Instead of using this we should update using a function as it will prevent the problem caused by scheduled rendering.
+        // setUserInput = (prevState) => {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // };
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+        //     setUserInput({ ...userInput, enteredDate: event.target.value });
+        //Instead of using this we should update using a function as it will prevent the problem caused by scheduled rendering.
+        // setUserInput = (prevState) => {
+        //     return { ...prevState, enteredDate: event.target.value };
+        // };
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+
+        setEnteredTitle("");
+        setEnteredDate("");
+        setEnteredAmount("");
+        props.onSaveExpenseData(expenseData);
+    };
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <div className="new-expense__controls">
+                    <div className="new-expense__control">
+                        <label>Title</label>
+                        <input
+                            value={enteredTitle}
+                            onChange={titleChangeHandler}
+                            type="text"
+                        />
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Amount</label>
+                        <input
+                            onChange={amountChangeHandler}
+                            type="number"
+                            min="0.01"
+                            step="0.01"
+                            value={enteredAmount}
+                        />
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Date</label>
+                        <input
+                            onChange={dateChangeHandler}
+                            type="date"
+                            min="2019-01-01"
+                            max="2022-12-31"
+                            value={enteredDate}
+                        />
+                    </div>
+                </div>
+                <div className="new-expense__actions">
+                    <button type="submit">Add Expense</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default ExpenseForm;
